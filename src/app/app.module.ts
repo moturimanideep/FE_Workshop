1ee2ed118c6494fab1315946c1f8dddc8ba5724f
krupavarma <krupavarma1995@gmail.com>
2018-02-07 18:10:13 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatCardModule} from '@angular/material/card';
-import { MenuComponent } from './menu/menu/menu.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+  
   ],
   imports: [
     BrowserModule,
