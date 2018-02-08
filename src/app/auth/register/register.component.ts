cdaf6942b8096e1127c54f56ea5f30030882882c
digitallync <digitallync@digitallyncs-MacBook-Air.local>
2018-02-08 18:14:14 +0530

    changes for login register setpassword

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+  workstatus = [
+    {value: 'Employee', viewValue: 'Employee'},
+    {value: 'Student', viewValue: 'Student'},
+    {value: 'Others', viewValue: 'Others'}
+  ];
+  technologies = [
+    {value: 'Front End ', viewValue: 'Front End'},
+    {value: 'Devops', viewValue: 'Devops'},
+    
+  ];
   constructor() { }
 
   ngOnInit() {
